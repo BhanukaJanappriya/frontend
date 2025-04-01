42ad6cc0b563eb69af7edfddc50719c8c3cf22ae
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-04-01 17:12:40 +0530

    Set routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import MyAppointments from "./pages/MyAppointments";
 
 const App = () => {
   return (
-    <div className='text-green-500'>Testing tailwind</div>
-  )
-}
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/" element={<Home />} />
 
-export default App
+
+
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
