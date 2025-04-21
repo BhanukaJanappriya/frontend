4f5dee86f8840109fa7f458214c58f3946f203b3
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-04-21 22:34:17 +0530

    Make header section of Home page

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      {/* {------------Left side------------} */}
+      <div>
+        <p>
+          Book Appointment
+          <br /> With Trusted Doctors
+        </p>
+        <div>
+          <img src={assets} alt="" />
+        </div>
+      </div>
+      {/* {------------Right side-----------} */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
